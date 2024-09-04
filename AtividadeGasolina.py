4d9d5906f665d177bc0e8cc0560ae3f8481355f9
AndreLuiz837 <andrelz9797@gmail.com>
2024-09-04 16:06:06 -0300

    Exercicio Gasolina

---
+++ AtividadeGasolina.py
@@ -0,0 +1,11 @@
+tipo = input(f"Qual o tipo do combustivel ('G' para gasolina e 'E' para etanol):")
+litros = float(input(f"Diga a quantidade de litros:"))
+
+if tipo == "G" or tipo == "E" or tipo == "g" or tipo == "e":
+    if tipo == "G" or tipo == "g":
+        valor = litros * 5.80
+    else:
+        valor = litros * 4.90
+    print(f"O valor a pagar sera {valor}.")
+else:
+    print(f"O tipo de combustivel é invalido.")
